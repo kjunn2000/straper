07f91ac506fb6e0b839c1694e79ba02063e7ef81
kjunn2000 <kjunn2000@gmail.com>
2021-12-28 12:40:03 +0800

    add refetch workspace data when reload

--- chat-web/src/page/Workspace.js
+++ chat-web/src/page/Workspace.js
@@ -1,15 +1,19 @@
-import { useEffect } from "react";
 import { useParams } from "react-router";
 import ChatRoom from "../components/ChatRoom";
 import Sidebar from "../components/sideBar/Sidebar";
 import useWorkspaceStore from "../store/workspaceStore";
 import {ReactComponent as Welcome} from "../asset/img/welcome.svg";
 import {ReactComponent as NotFound} from "../asset/img/notfound.svg";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useEffect } from "react";
+import { fetchWorkspaceData, redirectToLatestWorkspace } from "../service/workspace";
 
 function Workspace() {
   const { workspaceId, channelId } = useParams();
   const currWorkspace = useWorkspaceStore((state) => state.currWorkspace);
+
+  useEffect(() => {
+      fetchWorkspaceData().then(data => redirectToLatestWorkspace(data));
+  },[])
 
   const emptyComponent = (Image, text) => (
     <div className="flex flex-col items-center p-5 text-white">
---
+++ chat-web/src/service/workspace.js
@@ -0,0 +1,29 @@
+import api from "../axios/api";
+import useWorkspaceStore from "../store/workspaceStore";
+import history from "./history";
+
+export const fetchWorkspaceData = async () => {
+    const workspaceState = useWorkspaceStore.getState();
+    const res = await api.get("/protected/workspace/list");
+    if (res.data?.Success && res.data?.Data) {
+        workspaceState.setWorkspaces(res.data?.Data);
+        workspaceState.setDefaultSelectedChannelIds();
+        const selectedIds = [...workspaceState.selectedChannelIds]
+        if (selectedIds.length > 0) {
+        workspaceState.setCurrWorkspace(selectedIds[0][0])
+        workspaceState.setCurrChannel(selectedIds[0][1])
+        }
+    }
+    return res.data?.Data;
+};
+
+export const redirectToLatestWorkspace = (workspaces) => {
+    let redirectLink = "/channel";
+    if (workspaces.length > 0) {
+        redirectLink += "/" + workspaces[0].workspace_id;
+        if (workspaces[0].channel_list.length > 0) {
+        redirectLink += "/" + workspaces[0].channel_list[0].channel_id;
+        }
+    }
+    history.push(redirectLink);
+};
